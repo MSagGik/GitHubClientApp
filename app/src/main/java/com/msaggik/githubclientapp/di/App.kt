245bfd4dd9cf9ce7d0defd8b4bf12a890b6eeb63
MSagGik <smw.pantheon@bk.ru>
2024-05-15 13:13:24 +0300

    Make theme app

---
+++ app/src/main/java/com/msaggik/githubclientapp/di/App.kt
@@ -0,0 +1,42 @@
+package com.msaggik.githubclientapp.di
+
+import android.app.Application
+import android.content.SharedPreferences
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+
+private const val SHARED_PREFERENCES = "shared_preferences"
+private const val THEME_APP_KEY = "theme_app_key"
+class App : Application() {
+
+    private var lightTheme = true
+    private lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreate() {
+        super.onCreate()
+        sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE)
+        lightTheme = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE).getBoolean(THEME_APP_KEY, false)
+        setApplicationTheme(lightTheme)
+        Log.i("Start", "тема " + lightTheme)
+    }
+
+    fun setThemeSharedPreferences(themeEnabled: Boolean) {
+        lightTheme = themeEnabled
+        setApplicationTheme(lightTheme)
+        sharedPreferences.edit().putBoolean(THEME_APP_KEY, lightTheme).apply()
+    }
+
+    fun getApplicationTheme(): Boolean {
+        return lightTheme
+    }
+
+    private fun setApplicationTheme(themeEnabled: Boolean) {
+        AppCompatDelegate.setDefaultNightMode(
+            if(themeEnabled) {
+                AppCompatDelegate.MODE_NIGHT_NO
+            } else {
+                AppCompatDelegate.MODE_NIGHT_YES
+            }
+        )
+    }
+}
--- app/src/main/java/com/msaggik/githubclientapp/view/MainActivity.kt
+++ app/src/main/java/com/msaggik/githubclientapp/view/MainActivity.kt
@@ -1,40 +1,56 @@
 package com.msaggik.githubclientapp.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.msaggik.githubclientapp.R
+import com.msaggik.githubclientapp.di.App
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navigationController: NavController
     private lateinit var navigationHostFragment: NavHostFragment
+    private var isOnSetting = false
+    private lateinit var layoutSetting: FrameLayout
+    private lateinit var switchTheme: SwitchMaterial
+    private lateinit var fragmentContainer: FragmentContainerView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigationHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        layoutSetting = findViewById(R.id.layoutSetting)
+        switchTheme = findViewById(R.id.switchTheme)
+        fragmentContainer = findViewById(R.id.fragmentContainerView)
+        navigationHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navigationController = navigationHostFragment.navController
 
         val bottomNavigationViewMenu =
             findViewById<BottomNavigationView>(R.id.bottom_navigation_menu)
+
+        switchTheme.setChecked((applicationContext as App).getApplicationTheme())
+
+        switchTheme.setOnCheckedChangeListener { _, checked ->
+            (applicationContext as App).setThemeSharedPreferences(checked)
+        }
 
         // bottom navigation bar
         bottomNavigationViewMenu.setOnItemSelectedListener { item ->
 
             when (item.itemId) {
                 R.id.profile -> {
+                    isOnSetting = false
+                    visibleSetting(isOnSetting)
                     navigationController.currentDestination?.id?.let {
                         findNavController(R.id.fragmentContainerView).popBackStack(
                             it, true
@@ -51,6 +67,8 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 R.id.search -> {
+                    isOnSetting = false
+                    visibleSetting(isOnSetting)
                     navigationController.currentDestination?.id?.let {
                         findNavController(R.id.fragmentContainerView).popBackStack(
                             it, true
@@ -67,12 +85,8 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 R.id.settings -> {
-                    navigationController.currentDestination?.id?.let {
-                        findNavController(R.id.fragmentContainerView).popBackStack(
-                            it, true
-                        )
-                    }
-                    findNavController(R.id.fragmentContainerView).navigate(R.id.settingFragment)
+                    isOnSetting = true
+                    visibleSetting(isOnSetting)
                     true
                 }
                 else -> false
@@ -85,4 +99,29 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY_IS_SETTING, isOnSetting)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        isOnSetting = savedInstanceState.getBoolean(KEY_IS_SETTING, IS_SETTING_DEFAULT)
+        visibleSetting(isOnSetting)
+    }
+
+    private fun visibleSetting(checked: Boolean) {
+        if(checked) {
+            layoutSetting.visibility = View.VISIBLE
+            fragmentContainer.visibility = View.GONE
+        } else {
+            layoutSetting.visibility = View.GONE
+            fragmentContainer.visibility = View.VISIBLE
+        }
+    }
+
+    companion object {
+        const val KEY_IS_SETTING = "KEY_IS_SETTING"
+        const val IS_SETTING_DEFAULT = false
+    }
 }
