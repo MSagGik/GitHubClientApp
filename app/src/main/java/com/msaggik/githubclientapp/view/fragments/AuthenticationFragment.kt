474e8441f9c47e03b2da7e00bc3f8d6601ea8b3a
MSagGik <smw.pantheon@bk.ru>
2024-05-18 13:01:26 +0300

    Make refactor UI start fragment

--- app/src/main/java/com/msaggik/githubclientapp/view/fragments/AuthenticationFragment.kt
+++ app/src/main/java/com/msaggik/githubclientapp/view/fragments/AuthenticationFragment.kt
@@ -1,13 +1,25 @@
 package com.msaggik.githubclientapp.view.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.msaggik.githubclientapp.R
+import com.msaggik.githubclientapp.model.entities.itemsearch.Item
 
 class AuthenticationFragment : Fragment() {
+
+    private lateinit var context: Context
+
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +29,12 @@ class AuthenticationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_authentication, container, false)
+        val view = inflater.inflate(R.layout.fragment_authentication, container, false)
+
+        context = view.context
+
+        button = view.findViewById(R.id.button)
+
+        return view
     }
 }
